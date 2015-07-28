6ea33babd57e636457fee52b584590386cb2fb01
luhuajcdd <dehua@dehuadeMacBook-Pro.local>
2015-07-29 07:21:07 +0800

    提交联系人详情的pagerView结构

---
+++ app/src/main/java/com/parttime/addresslist/UserDetailPagerAdapter.java
@@ -0,0 +1,77 @@
+package com.parttime.addresslist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.qingmu.jianzhidaren.R;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+/**
+ *
+ * Created by dehua on 15/7/28.
+ */
+public class UserDetailPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<String> userIds;
+
+    public UserDetailPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void setData(LinkedHashSet<String> userIds){
+
+        this.userIds = new ArrayList<>(userIds);
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return UserDetailFragment.newInstance(userIds.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return userIds.size();
+    }
+
+    public static class UserDetailFragment extends Fragment{
+        private static final String ARG_USER_ID = "userId";
+        private String userId;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static UserDetailFragment newInstance(String userId) {
+            UserDetailFragment fragment = new UserDetailFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_USER_ID, userId);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public UserDetailFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_user_detail_item, container, false);
+            userId = getArguments().getString(ARG_USER_ID);
+            TextView tv = (TextView)rootView.findViewById(R.id.test);
+            tv.setText(userId);
+            return rootView;
+        }
+    }
+
+}
--- app/src/main/java/com/quark/jianzhidaren/BaseActivity.java
+++ app/src/main/java/com/quark/jianzhidaren/BaseActivity.java
@@ -1,9 +1,11 @@
 package com.quark.jianzhidaren;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +28,7 @@ import com.umeng.analytics.MobclickAgent;
  *
  * Created by Administrator on 10/31 0031.
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends FragmentActivity {
 	public static final String TAG = "com.quark";
 	protected TextView title;
 	protected Button right;
